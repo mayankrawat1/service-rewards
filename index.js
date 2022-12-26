3732ffe32220a3cf4e37c02fbba3026c7a01d9f3
mayank <mayank.rawat1@tothenew.com>
2022-12-26 13:02:57 +0530

    set mongodb connection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
