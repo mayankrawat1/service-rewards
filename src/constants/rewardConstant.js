43453664c9ec6e4e61d6e68b55af167423c7feb5
mayank <mayank.rawat1@tothenew.com>
2023-01-03 14:30:53 +0530

    chnage some reward_points and reward_badge in rewardConstant file

--- src/constants/rewardConstant.js
+++ src/constants/rewardConstant.js
@@ -3,9 +3,10 @@ const REWARD_POINTS = {
   "add-card": 10,
   "set-security-pin": 10,
   "deposit-money": 10,
-  visiting: 15,
-  "enable-blackbook-notification": 20,
+  "visit-venue": 10,
+  "visit-what-new-section": 15,
   "fingerprint-login": 10,
+  "verify-email": 5,
   "update-rg-tool": 10,
   "bestfriend-profile": 5
 };
@@ -16,11 +17,11 @@ const REWARD_BADGE = {
   3: "card-holder",
   4: "team-player",
   5: "blackbooked",
-  6: "visitor",
+  6: "promotor",
   7: "verified",
   8: "racing-punter",
   9: "sports-punter",
-  10: "SGM-punter"
+  10: "venue-punter"
 };
 
 const BADGE_POINT = {
@@ -29,15 +30,18 @@ const BADGE_POINT = {
   "card-holder": 10,
   "team-player": 10,
   blackbooked: 10,
-  visitor: 10,
+  promotor: 10,
   verified: 10,
   "racing-punter": 10,
   "sports-punter": 10,
-  "SGM-punter": 10
+  "venue-punter": 10
 };
 
 const SPIN_WHEEL = "spin-wheel";
 
 module.exports = {
-  REWARD_POINTS, REWARD_BADGE, BADGE_POINT, SPIN_WHEEL
+  REWARD_POINTS,
+  REWARD_BADGE,
+  BADGE_POINT,
+  SPIN_WHEEL
 };
